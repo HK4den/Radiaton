3a7abafd4b71c8b6ce32d42df02876ddfd8460d1
BandCade <cade.b.courtney07@gmail.com>
2024-10-25 09:46:17 -0500

    not working coorectly yet

---
+++ Radiaton/Assets/bullet/bullet_movemnet.cs
@@ -0,0 +1,21 @@
+using UnityEngine; 
+using System.Collections;
+
+public class shooting : MonoBehaviour 
+{
+	public float bulletSpeed = 10f;
+	public Rigidbody bullet;
+	
+	
+	void Fire()
+	{
+		Rigidbody bulletClone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
+		bulletClone.velocity = transform.forward * bulletSpeed;
+	}
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1"))
+			Fire();
+	}
+}
