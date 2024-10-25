4901c0d006700cd04b395877311b4aed2a948e8d
HK4den <kadenhuckf@gmail.com>
2024-10-25 10:42:00 -0500

    movement refined
    movements been fixed for the most part, can move now and added ways for it to be smoother.

--- Radiaton/Assets/Chmovement.cs
+++ Radiaton/Assets/Chmovement.cs
@@ -2,25 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chmovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public Rigidbody 2D rb;
-    Vector2 movement;
+    public float acceleration = 5f;
+    public float deceleration = 5f;
+    public float maxSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movementInput = Vector2.zero;
 
-    //update is called once per frame
-    private void Update()
+    void Start()
     {
-        //Input
-        movement.x =
-    Input.GetAxisRaw("Horizontal");
-        movement.y =
-    Input.GetAxisRaw("Vertical");
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Get input direction
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+        movementInput = movementInput.normalized;
+    }
 
+    void FixedUpdate()
+    {
+        // Calculate target velocity based on movement input
+        Vector2 targetVelocity = movementInput * moveSpeed;
+
+        // Determine whether to accelerate or decelerate
+        if (movementInput.magnitude > 0)
+        {
+            // Accelerate towards target velocity
+            rb.velocity = Vector2.MoveTowards(rb.velocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            // Decelerate to a stop if no input
+            rb.velocity = Vector2.MoveTowards(rb.velocity, Vector2.zero, deceleration * Time.fixedDeltaTime);
+        }
+
+        // Clamp velocity to max speed
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
     }
 }
