25afdb43b90152d75f9e9e909f15fe0f5df36ada
BandCade <cade.b.courtney07@gmail.com>
2024-11-08 09:12:06 -0600

    j

--- Radiaton/Assets/bullet/destroy_bullet.cs
+++ Radiaton/Assets/bullet/destroy_bullet.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class destroy_bullet : MonoBehaviour
+public class DestroyGameObject : MonoBehaviour
+private void OnCollisionEnter2D(Collision2D collision)
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    destroy(this.GameObject) 
 }
