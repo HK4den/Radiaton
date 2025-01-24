894c3f3bc6b0560cbf7d3a70de5f9e3efdceca3f
HK4den <kadenhuckf@gmail.com>
2025-01-24 10:24:32 -0600

    FIXED THE ENEMY MOVEMENT
    YAYYAYAYYYAYAY6YYAYAYTAYTAYTYAYAYYYAYYCFDGYSYGYYQAY

--- Radiaton/Assets/Enemy/aichase.cs
+++ Radiaton/Assets/Enemy/aichase.cs
@@ -4,32 +4,39 @@ using UnityEngine;
 
 public class aichase : MonoBehaviour
 {
-    public GameObject player;
+    public string targetTag = "Player"; // Tag to chase
     public float speed;
     public float distanceBetween;
 
-   private float distance;
+    private GameObject target;
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
-
     {
-        
+        // Find the target GameObject with the specified tag
+        target = GameObject.FindGameObjectWithTag(targetTag);
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
+        // Check if the target exists before proceeding
+        if (target == null)
+        {
+            Debug.LogWarning("Target with tag " + targetTag + " not found!");
+            return;
+        }
+
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        Vector2 direction = target.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-
-
-        if(distance > distanceBetween)
+        if (distance > distanceBetween)
         {
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
 }
