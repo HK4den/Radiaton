c578ce4513983139b26baf046874f18dec27b40f
BandCade <cade.b.courtney07@gmail.com>
2025-02-03 10:01:56 -0600

    i need to fix it

--- Radiaton/Assets/spin.cs
+++ Radiaton/Assets/spin.cs
@@ -8,6 +8,6 @@ public class spin : MonoBehaviour
 
     void update()
     {
-         transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+         transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
     }
 }
