069af19822449811f647ef56721d4b2f0f8b9679
HK4den <kadenhuckf@gmail.com>
2025-02-04 08:34:52 -0600

    Hrealth

--- Radiaton/Assets/Enemy/EnemyHealth.cs
+++ Radiaton/Assets/Enemy/EnemyHealth.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxHealth = 5;
+    private int currentHealth;
+
+    public AudioClip deathSound; // Optional death sound
+    private AudioSource audioSource;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        audioSource = Camera.main.GetComponent<AudioSource>(); // Use camera's AudioSource
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.CompareTag("Playerbullet") || collider.CompareTag("FutureTag"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Enemy took damage! Current HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        // Play death sound if available
+        if (audioSource != null && deathSound != null)
+        {
+            audioSource.PlayOneShot(deathSound);
+        }
+
+        Destroy(gameObject); // Remove the enemy from the scene
     }
 }
