0f97e34095765d7cd6d448195d85443be91f3fd8
HK4den <kadenhuckf@gmail.com>
2025-03-30 21:43:07 -0500

    AAA

--- Radiaton/Assets/Scripts/Pause Manager.cs
+++ Radiaton/Assets/Scripts/Pause Manager.cs
@@ -4,8 +4,22 @@ using UnityEngine.SceneManagement;
 public class PauseManager : MonoBehaviour
 {
     public GameObject pauseMenuUI;
+    public GameObject player; // Assign player GameObject in Inspector
 
     private bool isPaused = false;
+    private DashToMouse dashScript;
+    private PlayerMovement moveScript;
+    private shooting shootScript;
+
+    void Start()
+    {
+        if (player != null)
+        {
+            dashScript = player.GetComponent<DashToMouse>();
+            moveScript = player.GetComponent<PlayerMovement>();
+            shootScript = player.GetComponent<shooting>();
+        }
+    }
 
     void Update()
     {
@@ -27,6 +41,13 @@ public class PauseManager : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
+        if (player != null)
+        {
+            dashScript.enabled = false;
+            moveScript.enabled = false;
+            shootScript.enabled = false;
+        }
     }
 
     public void ResumeGame()
@@ -34,20 +55,24 @@ public class PauseManager : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+
+        if (player != null)
+        {
+            dashScript.enabled = true;
+            moveScript.enabled = true;
+            shootScript.enabled = true;
+        }
     }
 
-    public void Quit()
+    public void QuitGame()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
+        Debug.Log("Quitting Game...");
         Application.Quit();
-#endif
     }
 
     public void LoadMainMenu()
     {
-        Time.timeScale = 1f; // Make sure time resumes
-        SceneManager.LoadScene("MainMenu"); // Replace with your main menu scene name
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
