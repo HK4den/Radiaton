567a42544205913e4de163c18ce9aa2a523a0ed2
BandCade <cade.b.courtney07@gmail.com>
2025-04-09 08:57:21 -0500

    Update heal.cs

--- Radiaton/Assets/heal.cs
+++ Radiaton/Assets/heal.cs
@@ -17,7 +17,7 @@ public class heal : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
 {
-   if(collision.gameObject.name == "HealSlug")
+   if(collision.gameObject.name == "Heal Slug")
    {
         Destroy(collision.gameObject);
    }
